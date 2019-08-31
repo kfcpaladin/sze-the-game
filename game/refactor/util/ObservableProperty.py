0f9b0360f149a1e48c6a8dc7b4f2bd4f7f441cea
William Yang <williamyang2017@gmail.com>
2019-09-01 02:01:25 +1000

    Modified observer to use __call__ overload

--- game/refactor/util/ObservableProperty.py
+++ game/refactor/util/ObservableProperty.py
@@ -2,6 +2,7 @@ class ObservableProperty(object):
     def __init__(self, value):
         self._value = value
         self._observers = [] 
+        self._binds = []
 
     @property
     def value(self):
@@ -11,12 +12,16 @@ class ObservableProperty(object):
     def value(self, _val):
         old_val = self._value
         self._value = _val
-        self._on_change(old_val, _val)
+        if old_val != _val:
+            self._on_change(old_val, _val)
 
     def observe(self, observer):
         self._observers.append(observer)
         return observer
-    
+
+    def bind(self, other_prop):
+        self._binds.append(other_prop)
+
     def detach(self, observer):
         try:
             self._observers.remove(observer)
@@ -26,4 +31,7 @@ class ObservableProperty(object):
 
     def _on_change(self, old, new):
         for observer in self._observers:
-            observer.on_change(old, new)
+            observer(old, new)
+            # observer.on_change(old, new)
+        for other_prop in self._binds:
+            other_prop.value = new

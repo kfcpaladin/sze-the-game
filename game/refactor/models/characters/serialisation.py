55006b3af63b83965025e18f78ca82c88522f80d
William Yang <williamyang2017@gmail.com>
2019-08-31 16:57:23 +1000

    Move notifications into separate folder

--- game/refactor/models/characters/serialisation.py
+++ game/refactor/models/characters/serialisation.py
@@ -1,7 +1,5 @@
 from refactor.persistence import JSONImporter, JSONExporter
 from .Character import Character
-from .Attribute import Attribute
-from .AttributeDependency import AttributeDependency
 
 @JSONImporter.register("character")
 def import_character(data):
@@ -18,24 +16,6 @@ def import_character(data):
 
     JSONImporter.cache_object(("character", character.name), character)
     return character
-
-@JSONImporter.register("attribute")
-def import_attribute(data):
-    name = data.get("name")
-    value = data.get("value")
-    return Attribute(name, value)
-
-@JSONImporter.register("attribute_dependency")
-def import_attribute_dependency(data):
-    character_name = data.get("character_name")
-    attribute_name = data.get("attribute")
-    condition_data = data.get("condition")
-
-    character = JSONImporter.fetch_cached_object(("character", character_name))
-    attribute = character.get_attribute(attribute_name)
-    condition = JSONImporter.visit("condition", condition_data)
-
-    return AttributeDependency(attribute, condition)
 
 
 @JSONExporter.register("character")
@@ -54,10 +34,4 @@ def export_character(character):
 
     return data
 
-@JSONExporter.register("attribute")
-def export_attribute(attribute):
-    data = {}
-    data.setdefault("name", attribute.name)
-    data.setdefault("value", attribute.value)
 
-    return data

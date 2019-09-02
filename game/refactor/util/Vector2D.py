9970b7609c4a67c5d9605ab888713502b081d01b
William Yang <williamyang2017@gmail.com>
2019-09-02 21:52:21 +1000

    Added Vector2D class

---
+++ game/refactor/util/Vector2D.py
@@ -0,0 +1,22 @@
+class Vector2D(object):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def __neg__(self):
+        return Vector2D(-self.x, -self.y)
+    
+    def __pos__(self):
+        return self
+    
+    def __add__(self, v):
+        return Vector2D(self.x+v.x, self.y+v.y)
+    
+    def __sub__(self, v):
+        return Vector2D(self.x-v.x, self.y-v.y)
+    
+    def __mul__(self, k):
+        return Vector2D(k*self.x, k*self.y)
+    
+    def __div__(self, k):
+        return Vector2D(self.x/k, self.y/k)
